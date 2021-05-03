a1e90af5e742d264a309c69d2abb77879c457d1e
GasinAn <Gasin185@163.com>
2021-05-04 01:35:48 +0800

    test, 85, 628, 2.75

--- test.py
+++ test.py
@@ -89,29 +89,35 @@ def test_():
 def test():
     """Test reduce_precision for Tianlai data."""
 
+    import os
     import time
 
     f = 0.01 # Precision reduction parameter.
     N = 100  # Number of samples integrated (delta_f*delta_t).
 
-    t_s = time.perf_counter()
+    fsize = os.path.getsize('example.hdf5')
 
+    t_s = time.perf_counter()
     with h5py.File('example.hdf5', 'r') as df:
         vis, blorder = df['vis'][...], df['blorder'][...]
-
     reduce_precision(vis, blorder, f/N)
-
     with h5py.File('example.bs.hdf5', 'w') as df:
         df.create_dataset('vis', data=vis, **hdf5plugin.Bitshuffle())
-
     t_e = time.perf_counter()
 
-    import os
-    fsize = os.path.getsize('example.hdf5')
-    bs_fsize = os.path.getsize('example.bs.hdf5')
-    print((fsize/1024**2)/(t_e-t_s))
-    print((bs_fsize/fsize)*100)
+    print("Throughput(compress): %f MiB/s" %((fsize/1024**2)/(t_e-t_s)))
 
+    del vis
+
+    t_s = time.perf_counter()
+    with h5py.File('example.bs.hdf5', 'r') as df:
+        vis = df['vis'][...]
+    t_e = time.perf_counter()
+
+    print("Throughput(decompress): %f MiB/s" %((fsize/1024**2)/(t_e-t_s)))
+
+    bs_fsize = os.path.getsize('example.bs.hdf5')
+    print('Compression rate: %f %%' %((bs_fsize/fsize)*100))
 
 if __name__=='__main__':
     test()
