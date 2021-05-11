134cbbc8832e2ce8dd273800555e7f44671aa707
GasinAn <Gasin185@163.com>
2021-05-11 22:31:30 +0800

    make fpath and fparam stdin (as example.txt)

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import hdf5plugin
 from dnb import reduce_precision
 
 
-EXAMPLE = '/DATALINKS/D190611_3srcNP/3srcNP_20190622023112_20190622033112.hdf5'
+N = 244140 # Number of samples integrated (delta_f*delta_t).
 
 
 def test():
@@ -14,13 +14,13 @@ def test():
     import os
     import time
 
-    f = 0.01   # Precision reduction parameter.
-    N = 244140 # Number of samples integrated (delta_f*delta_t).
+    example = input('HDF5 file path: ')
+    f = float(input('Precision reduction parameter f: '))
 
-    fsize = os.path.getsize(EXAMPLE)
+    fsize = os.path.getsize(example)
 
     t_s = time.perf_counter()
-    with h5py.File(EXAMPLE, 'r') as df:
+    with h5py.File(example, 'r') as df:
         vis, blorder = df['vis'][...], df['blorder'][...]
     reduce_precision(vis, blorder, f/N)
     with h5py.File('example.bs.hdf5', 'w') as df:
@@ -39,7 +39,7 @@ def test():
     print("Throughput(decompress): %f MiB/s" %((fsize/1024**2)/(t_e-t_s)))
 
     bs_fsize = os.path.getsize('example.bs.hdf5')
-    print('Compression rate: %f %%' %((bs_fsize/fsize)*100))
+    print("Compression rate: %f %%" %((bs_fsize/fsize)*100))
 
 
 if __name__=='__main__':
